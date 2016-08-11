610b737ef2185f9ee94a8f84cdfb6ff5c12d4465
Peter <peter@atomjump.com>
2016-08-12 11:38:49 +1200

    testing

--- index.php
+++ index.php
@@ -31,25 +31,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
    
 
-    <!-- AtomJump Feedback Starts -->
-   <!-- Bootstrap core CSS. Ver 3.3.1 sits in css/bootstrap.min.css -->
-	  <link rel="StyleSheet" href="<?php echo $bootstrap_css_path ?>/bootstrap.min.css" rel="stylesheet">
-	
-	<!-- AtomJump Feedback CSS -->
-	<link rel="StyleSheet" href="<?php echo $atomjump_path ?>/css/comments-0.1.css?ver=1">
-
-
-
-
-	
-	<!-- Bootstrap HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
-	
-	
-	
     <style>
     
         html, body {
@@ -84,11 +65,30 @@
 			font-family: <?php echo $font_family ?> !important;
 			text-decoration: none;
 		}
-
-  
-        
-    
+   
     </style>
+
+
+    <!-- AtomJump Feedback Starts -->
+   <!-- Bootstrap core CSS. Ver 3.3.1 sits in css/bootstrap.min.css -->
+	  <link rel="StyleSheet" href="<?php echo $bootstrap_css_path ?>/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- AtomJump Feedback CSS -->
+	<link rel="StyleSheet" href="<?php echo $atomjump_path ?>/css/comments-0.1.css?ver=1">
+
+
+
+
+	
+	<!-- Bootstrap HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	
+	
+	
+
 
 	
 	<script>
@@ -117,14 +117,10 @@
             
             <div class="col-md-12">
                 <div class="centering text-center">
-             
-                   
-                   
-                   
+                 
                    <span id="my-comments" class="comment-open" style="display: none;" href="javascript:">Click me for comments</span>
 		            <!-- Any link on the page can have the 'comment-open' class added and a blank 'href="javascript:"' -->
-		
-		            
+	            
                    
 					<div style="position: relative; width:100%; height: 768px; margin-left: auto; margin-right:auto">
 						<div id="my_canvas" style="width:100%; height: 768px; "></div>
@@ -168,9 +164,7 @@
              </div>
           
            </div> <!-- end of col -->
-           <div class="col-md-2"></div>
-          
-            
+           <div class="col-md-2"></div>            
           
         </div> <!-- end of row -->
         
@@ -208,7 +202,7 @@
 			
 		} else {
 			$('#show-mobile-display').hide();
-			$('#forum').css('width: 80%');
+			$('#forum').css('width', '80%');
 		}
 
 
