6976568d96d375e9d6e12c8c0ebd43d1bd1ab9a3
Peter <peter@atomjump.com>
2016-08-12 11:09:33 +1200

    testing

--- index.php
+++ index.php
@@ -229,7 +229,7 @@
 		    	var weight = list[cnt][1];
 		    	var col = getColor(word, weight);
 		    	
-		    	var fontSize = Math.pow(weight, 3) * $('#mobile-display').width() / 1024;
+		    	var fontSize = Math.pow(weight, 2.4) * $('#mobile-display').width() / 1024;
 		    	all = all + "<a href='javascript:' onclick='return clickEntry(words.list[" + cnt + "], null);' style='color: " + col + "; font-size:" + fontSize + "px'; text-decoration: none;>" + word + "</a></br>";
 		    	
 		    }   
