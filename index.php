b5fcc33b0362b4fd17102e71a676e6def2fa4307
Peter <peter@atomjump.com>
2016-08-12 10:22:14 +1200

    testing

--- index.php
+++ index.php
@@ -212,7 +212,9 @@
 		    	
 		    	var fontSize = Math.pow(weight, 2.3) * $('#my_canvas').width() / 1024;
 		    	all = all + "<a href='javascript:' onclick='return clickEntry(" + list[cnt] + ", null);' style='color: " + col + "; font-size:" + fontSize + "px'>" + word + "</a></br>";
+		    	
 		    }   
+		    alert(all);
 		    
 		    $('#mobile-display').html(all);         
             	
