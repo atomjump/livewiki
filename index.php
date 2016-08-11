f279151670928dd65835be245d1b83f241d4fff1
Peter <peter@atomjump.com>
2016-08-12 11:57:24 +1200

    testing

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
                             </select>
                         </div>
                     </div> <!-- end of input group -->
-                    <button style="margin-top: 4px;" type="submit" class="btn btn-primary">Submit</button>
+                    <button id="submit-button" type="submit" class="btn btn-primary">Submit</button>
                   </div>  <!-- end of form group -->
 
                   
@@ -217,6 +217,7 @@
 			//Mobile version
 			$('#show-word-cloud').hide();
 			$('#forum').css('width', '300px;');
+			$('#submit-button').css('margin-top', '4px'); 
 			
 		} else {
 			$('#show-mobile-display').hide();
