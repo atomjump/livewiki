2eb927090fbea9e7d2015ff91b42f4523c5f3cbf
AtomJump <peter@atomjump.com>
2016-09-27 15:46:04 +1300

    backdrop

--- index.php
+++ index.php
@@ -160,7 +160,8 @@
             
             <div class="col-md-12">
                 <div class="centering text-center">
-                    <div id="mobile-display" style="background-color:#FFF; padding: 10px; opacity: 0.8;"></div>
+                	<div id="mobile-display" style="background-color:#FFF; padding: 10px; opacity: 0.8;"></div>
+                    <div id="mobile-display" style="position: absolute; top: 10px;"></div>
                  </div>
             </div>
             
