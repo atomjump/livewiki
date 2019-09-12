683424680668715e45c4d7861a2dbc95b8a5297c
AtomJump <peter@atomjump.com>
2019-09-13 08:24:27 +1200

    include all dependencies

--- configORIGINAL.php
+++ configORIGINAL.php
@@ -13,15 +13,15 @@
 	$background_color = "#5ca7d2";                                  //Background colour
 	$font_family = "Times, serif";
   
-	$bootstrap_css_path = "https://atomjump.com/bower_components/bootstrap/dist/css/bootstrap.min.css";  	//Put relative/absolute URL to your bootstrap css file
-	$atomjump_js_path = "https://atomjump.com/js/chat-0.9.2.js"; 					//Put relative/absolute URL to your AtomJump Loop Javascript chat.js file
-	$atomjump_css_path = "https://atomjump.com/bower_components/atomjump/css/releases/comments-0.9.1.css?ver=1";              //Put relative/absolute URL to your AtomJump Loop Javascript comments.css file
+	$bootstrap_css_path = "css/bootstrap.min.css";  	//Put relative/absolute URL to your bootstrap css file
+	$atomjump_js_path = "js/chat-0.9.2.js"; 					//Put relative/absolute URL to your AtomJump Loop Javascript chat.js file
+	$atomjump_css_path = "css/comments-0.9.1.css?ver=1";              //Put relative/absolute URL to your AtomJump Loop Javascript comments.css file
 	$my_machine_user = "1.1.1.1:2";                   								//Main owner of site. No longer so relevant.                                       
-	$wordcloud_js_path = "node_modules/wordcloud/src/wordcloud2.js";			//Relative or absolute path to the wordcloud js file
-	$jquery_js_path = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js";	//Relative or absolute path to the jquery js file. Ver 1.12.0 is know to be compatible.
+	$wordcloud_js_path = "js/wordcloud2.js";			//Relative or absolute path to the wordcloud js file
+	$jquery_js_path = "js/jquery.min.js";	//Relative or absolute path to the jquery js file. Ver 1.12.0 is know to be compatible.
 
-	$html5shiv_path = "https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js";		//Relative or absolute path to IE handling HTML5
-	$respond_path = "https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js";		//Relative or absolute path to IE handling HTML5
+	$html5shiv_path = "js/html5shiv.js";		//Relative or absolute path to IE handling HTML5
+	$respond_path = "js/respond.min.js";		//Relative or absolute path to IE handling HTML5
 
 
 ?>
