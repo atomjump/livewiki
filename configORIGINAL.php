5166f42812c0155462b6bcba3e6b1851dbe86e0e
AtomJump <peter@atomjump.com>
2019-09-13 08:26:59 +1200

    include all dependencies

--- configORIGINAL.php
+++ configORIGINAL.php
@@ -1,11 +1,7 @@
 <?php
 	$server = "http://yourserver.com/atomjump-loop-server/";        //Put URL to your AtomJump Loop Server install
 
-	if($_SERVER['SERVER_NAME'] == '127.0.0.1') {
-		$host = "http://127.0.0.1/";                                //Localhost option, for development
-	} else {
-		$host = "http://yoursite.com";                              //Live site, put your client website address in here
-	}
+    $host = "http://" . $_SERVER['HTTP_HOST'] . "/livewiki/";       //LiveWiki site, put your client website address in here
 
 	$unique_key = "yoursite_";                                      //This must be set to your site or business name,
 																	//and it should be globally unique to the $server
