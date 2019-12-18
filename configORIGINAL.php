b62963a4d16de0476674d8c39297c80153419518
AtomJump <peter@atomjump.com>
2019-12-18 14:48:52 +1300

    with auto-generated titles

--- configORIGINAL.php
+++ configORIGINAL.php
@@ -11,7 +11,7 @@
 	$font_family = "Times, serif";
   
 	$bootstrap_css_path = "css/bootstrap.min.css";  	//Put relative/absolute URL to your bootstrap css file
-	$atomjump_js_path = "js/chat-1.0.5.js"; 					//Put relative/absolute URL to your AtomJump Loop Javascript chat.js file
+	$atomjump_js_path = "js/chat-1.0.6.js"; 					//Put relative/absolute URL to your AtomJump Loop Javascript chat.js file
 	$atomjump_css_path = "css/comments-1.0.1.css?ver=1";              //Put relative/absolute URL to your AtomJump Loop Javascript comments.css file
 	$my_machine_user = "1.1.1.1:2";                   								//Main owner of site. No longer so relevant.                                       
 	$wordcloud_js_path = "js/wordcloud2.js";			//Relative or absolute path to the wordcloud js file
