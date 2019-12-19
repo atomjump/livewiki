cbd73586c2eb0361e5141fcd3ecf6f540ac64e9e
AtomJump <peter@atomjump.com>
2019-12-19 14:33:50 +1300

    with newer css

--- configORIGINAL.php
+++ configORIGINAL.php
@@ -12,7 +12,7 @@
   
 	$bootstrap_css_path = "css/bootstrap.min.css";  	//Put relative/absolute URL to your bootstrap css file
 	$atomjump_js_path = "js/chat-1.0.7.js"; 					//Put relative/absolute URL to your AtomJump Loop Javascript chat.js file
-	$atomjump_css_path = "css/comments-1.0.1.css?ver=1";              //Put relative/absolute URL to your AtomJump Loop Javascript comments.css file
+	$atomjump_css_path = "css/comments-1.0.3.css?ver=1.0.3";              //Put relative/absolute URL to your AtomJump Loop Javascript comments.css file
 	$my_machine_user = "1.1.1.1:2";                   								//Main owner of site. No longer so relevant.                                       
 	$wordcloud_js_path = "js/wordcloud2.js";			//Relative or absolute path to the wordcloud js file
 	$jquery_js_path = "js/jquery.min.js";	//Relative or absolute path to the jquery js file. Ver 1.12.0 is know to be compatible.
